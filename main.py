b0e3a378eade63a4ae4e350b6878b40605f072e6
Erik Vanderwerf <eski787@gmail.com>
2021-03-03 21:44:51 -0800

    Setting TOKEN in file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from pathlib import Path
+
 from slacksport.export import JsonSlackExport
 
-TOKEN = ''
-
-out = JsonSlackExport('erik', TOKEN)
+TOKEN = Path('TOKEN').read_text('utf-8').strip()
+export = JsonSlackExport(Path('export'), TOKEN)
